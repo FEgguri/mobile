aa18ac95718b1d8556e973c9113fc332c043c3ef
leeys <95343772+agent-megurii@users.noreply.github.com>
2022-01-11 14:47:53 +0900

    Add files via upload

--- mobile0.1/js/index.js
+++ mobile0.1/js/index.js
@@ -1,2 +1,17 @@
 
 //메인 컨텐츠 슬라이더
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
