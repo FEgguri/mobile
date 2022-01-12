c0699deb78a7c740afcbf662cc1cdebdaef743af
leeys <95343772+agent-megurii@users.noreply.github.com>
2022-01-12 09:20:21 +0900

    Add files via upload

--- mobile0.1/js/common.js
+++ mobile0.1/js/common.js
@@ -7,11 +7,13 @@ $('body')
 
 $('header').load('js/hdft.html header',navi);
 $('.category-w').load('js/hdft.html .category-w ul');
-$('.clickaside-popup').load('js/hdft.html .clickaside-popup .popupbox');
+$('.clickaside-popup').load('js/hdft.html .clickaside-popup .popupbox',popup);
 $('footer').load('js/hdft.html footer .footerbox');
 
 
 function navi(){
+
+  
     //popup-on
     $('.burger').on('click',function(){
         $('.clickaside-popup').addClass('active')
@@ -19,11 +21,16 @@ function navi(){
         
     })
 
-    //popup-close
-    $('.close').on('click',function(){        
-        $('.clickaside-popup').removeClass('active')
-        $('body').css('overflow','visible')
-    })
+
   
 }
 
+function popup(){
+      //popup-close
+      $('.close').on('click',function(){        
+        $('.clickaside-popup').removeClass('active')
+        $('body').css('overflow','visible')
+        
+        
+    })
+}
